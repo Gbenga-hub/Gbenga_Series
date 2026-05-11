353d5624a148e1a5e91f933e787a10b9175ba8b9
Gbenga <idowugbenga018@gmail.com>
2026-05-11 08:38:52 -0500

    Added a new line of code to first python file

--- First_file.py
+++ First_file.py
@@ -5,3 +5,6 @@ print("Hello, welcome to my first commit in git series")
 
 # Add a new line for code from branch
 Print("Learnt to branch in git in my Git Series")
+
+# Add a new line of code from master branch
+print("This is the master branch and I am learning git in my Git series")
